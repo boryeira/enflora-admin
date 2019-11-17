dd1068ec05710bb06bb4258e590f58a02267a963
boryeira <jmanuel.jorquera@gmail.com>
2019-11-17 20:17:24 -0300

    mail 2

---
+++ resources/views/mails/orders/pay.blade.php
@@ -0,0 +1,13 @@
+<div>
+  holaaaa
+  {{-- <img src="{{asset('img/enflora.png')}}" width="150"/>
+  <p>
+    Cantidad:{{$order->quantity}}<br />
+    total:{{$order->amount}}
+  </p>
+  @foreach ($items as $item)
+    <p>
+      item {{$item->quantity}}
+    </p>
+  @endforeach --}}
+</div>
